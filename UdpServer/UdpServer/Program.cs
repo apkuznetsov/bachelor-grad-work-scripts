5f78b5ad649ba298c4cf9373f5518e6cad47abb9
Leonid Kurepin <leonidkurepin@yandex.ru>
2020-05-08 22:59:21 +0400

    #SensorsConnection: Improve UDP server to allow define sending IP and port

--- UdpServer/UdpServer/Program.cs
+++ UdpServer/UdpServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,13 +7,22 @@ namespace UdpServer
 {
     class Program
     {
-        private static int LOCAL_PORT = 8888;
-        static string REMOTE_ADDRESS = "127.0.0.1"; // IP-address of the target listener
-        static int REMOTE_PORT = 8888; // Port of the target listener
+        private static string _sendingIpAddress = "127.22.22.1";
+        // private static string _sendingIpAddress = "127.0.0.1";
+        private static int _sendingPort = 6969;
+
+        static string TARGET_ADDRESS = "127.0.0.1"; // IP-address of the target listener
+        static int TARGET_PORT = 8888; // Port of the target listener
 
         static void Main(string[] args)
         {
-            try
+            // Console.Write("Enter ip address to send from: ");
+            // _sendingIpAddress = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter port to send from: ");
+            _sendingPort = int.Parse(Console.ReadLine());
+
+        try
             {
                 SendMessage();
             }
@@ -23,18 +33,29 @@ namespace UdpServer
         }
         private static void SendMessage()
         {
-            UdpClient sender = new UdpClient();
+            if (!IPAddress.TryParse(_sendingIpAddress, out IPAddress sendingIpAddress))
+            {
+                throw new FormatException("Invalid ip-adress");
+            }
 
+            IPEndPoint localIpEndPoint = new IPEndPoint(sendingIpAddress, _sendingPort);
+
+            UdpClient sender = new UdpClient(localIpEndPoint);
+
+            Console.WriteLine($"You\'re sending from: {localIpEndPoint.Address}:{localIpEndPoint.Port}");
+            
             try
             {
                 while (true)
                 {
+                    Console.Write("Type message to send: ");
+
                     string message = Console.ReadLine();
 
                     // byte[] encodedMessage = Encoding.Unicode.GetBytes(message);
                     byte[] encodedMessage = Encoding.ASCII.GetBytes(message);
 
-                    sender.Send(encodedMessage, encodedMessage.Length, REMOTE_ADDRESS, REMOTE_PORT);
+                    sender.Send(encodedMessage, encodedMessage.Length, TARGET_ADDRESS, TARGET_PORT);
                 }
             }
             catch (Exception ex)
